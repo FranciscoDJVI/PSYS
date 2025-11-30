05ad296a1be4f5709d66ad93db522d678789c26b
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-11-30 18:18:38 -0500

    created models
    Created models and add custom user in settings

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,57 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    sizes = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Sell(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        CONFIRMED = "Confirmed"
+        CANCELED = "Canceled"
+
+    # ID unique in time and space.
+    sell_id = models.UUIDField(uuid=uuid.uuid4)
+    status = models.TextChoices(
+        value=StatusChoices.choices, default=StatusChoices.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Relation with model User ForeignKey.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Relation with model Product of ManyToMany.
+    products = models.ManyToManyField(
+        Product, through="Sell_item", related_name="sells"
+    )
+
+    def __str__(self) -> str:
+        return str(self.sell_id)
+
+
+class SellItem(models.Model):
+    sell = models.ForeignKey(Sell, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    @property
+    def sell_subtotal(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.sell} {self.product}"
