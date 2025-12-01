285ec80f7910c08af48425b5d2f4371479a16856
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-11-30 20:26:47 -0500

    add permission

--- backend/api/views.py
+++ backend/api/views.py
@@ -19,7 +19,19 @@ class SellItemViewSet(viewsets.ModelViewSet):
     queryset = SellItem.objects.all()
     serializer_class = SellItemSerialiazer
 
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == "POST":
+            self.permission_classes = [IsAdminUser, IsAuthenticated]
+        return super().get_permissions()
+
 
 class SellViewSet(viewsets.ModelViewSet):
     queryset = Sell.objects.all()
     serializer_class = SellSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == "POST":
+            self.permission_classes = [IsAdminUser, IsAuthenticated]
+        return super().get_permissions()
