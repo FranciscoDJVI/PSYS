85d4c2d7b93efeb64ef46ab75e76852ac2c9418b
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-11-30 19:18:56 -0500

    add sellSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Product, SellItem
+from api.models import Product, Sell, SellItem
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -21,4 +21,23 @@ class SellItemSerialiazer(serializers.ModelSerializer):
             "product_price",
             "quantity",
             "sell_subtotal",
+        )
+
+
+class SellSerializer(serializers.ModelSerializer):
+    sell_id = serializers.UUIDField(read_only=True)
+    # Raltionship with SellItemSerialiazer for  many sellitems.
+    sell_items = SellItemSerialiazer(many=True, required=False)
+
+    def total(self, obj):
+        sell_items = obj.objects.all()
+        return sum(sell_item.item_subtotal for sell_item in sell_items)
+
+    class Meta:
+        model = Sell
+        fields = (
+            "sell_id",
+            "status",
+            "created_at",
+            "sell_items",
         )
