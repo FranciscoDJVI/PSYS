59795258ffaae4e71ffcdaaaaa6b412617c5e6f5
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-11-30 20:02:04 -0500

    add permissions
    add permission AllowAny and IsAdminUser

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,18 @@
 from rest_framework import viewsets
 from api.models import Product, SellItem, Sell
 from api.serializers import ProductSerializer, SellItemSerialiazer, SellSerializer
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == "POST":
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 class SellItemViewSet(viewsets.ModelViewSet):
